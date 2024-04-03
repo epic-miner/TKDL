580488fb89a69860d76211d19293172fb4c3a634
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-04-03 13:41:45 +0100

    Add files via upload

---
+++ tiktok.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install requirements
+pip install pystyle
+pip install requests 
+pip install inquirer
+pip install colorama
+pip install moviepy
+pip install rich
+pip install requests-html
+pip install requests-random-user-agent
+
+# Run main.py
+python main.py
